3f86f4d1e49601320a51a8929ac6a9ab806e303c
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2022-01-09 22:08:42 +0300

    added day 4

---
+++ Challenges/Day 4 - Class vs. Instance.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+ 
+for x in range(int(input())):
+    N = input()
+    print(N[::2],N[1::2])
