a57a3282fab0e948baa4c57212b45b595804bfc1
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2022-01-09 21:41:41 +0300

    added day 3

---
+++ Challenges/Day 3 - Intro to Conditional Statements.py
@@ -0,0 +1,26 @@
+# https://www.hackerrank.com/challenges/30-conditional-statements/problem?
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+        
+                             
+        
+if __name__ == '__main__':
+    N = int(input().strip())
+if N % 2 == 0:
+    if N <= 5 and N > 2:
+        print('Not Weird')
+    elif N > 6 and N <= 20:
+        print('Weird')
+    elif N > 20:
+        print('Not Weird')
+elif N % 2 == 1 :
+    print('Weird')
