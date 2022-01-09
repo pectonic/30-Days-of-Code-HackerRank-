afa529ad03fa4cb6ffca66ef93b0907e6fce9e38
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2022-01-09 21:42:06 +0300

    added day 5

---
+++ Challenges/Day 5 - Loops.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/30-loops/problem? 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+      
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for x in range(10):
+        print(n,"x",(x+1),"=",(n*(x+1)))
+
+    
