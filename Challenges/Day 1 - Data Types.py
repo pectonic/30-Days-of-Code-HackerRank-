d08e932c6737b89eaa4ffd30560e3a58d81943dd
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2022-01-09 21:41:26 +0300

    added day 1

---
+++ Challenges/Day 1 - Data Types.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/30-data-types/problem 
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+i_unput = int(input())
+d_unput = float(input())
+s_unput = str(input())
+
+print(i+i_unput)
+print(d+d_unput)
+print(s+s_unput)
+
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
