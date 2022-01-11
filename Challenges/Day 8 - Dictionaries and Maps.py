c585273ad2ad5812cc871edc020b9ef59a5d5470
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2022-01-11 21:48:54 +0300

    added day 8

---
+++ Challenges/Day 8 - Dictionaries and Maps.py
@@ -0,0 +1,17 @@
+n = 3
+d = {}
+keys = []
+values = []
+
+deneme1 = ["deneme", 'jea', 'lo']
+deneme2 = [ 32132,421421,521521]
+
+for i in range(n):
+    d[deneme1[i]] = deneme2[i]
+
+
+
+print(d)
+
+print(d.keys())
+
